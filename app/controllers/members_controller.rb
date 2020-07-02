8e51a7f1847ac0724fea83124e49268e35ef6e0c
vagrant <vagrant@localhost.localdomain>
2020-07-02 07:48:18 +0000

    [Add] マイページと編集機能を追加する

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,11 +3,27 @@ class MembersController < ApplicationController
   end
 
   def show
+  	@member = Member.find(params[:id])
   end
 
   def edit
+  	@member = Member.find(params[:id])
+  end
+
+  def update
+  	@member = Member.find(params[:id])
+  	if @member.update(member_params)
+		redirect_to member_path(@member)
+	else
+		render :edit
+	end
   end
 
   def confirm
+  end
+
+  private
+  def member_params
+  	params.require(:member).permit(:profile_image, :name, :school_year, :email)
   end
 end
