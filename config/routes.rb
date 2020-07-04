32e8f69d48852f166bf5f7fb63205672930fc646
vagrant <vagrant@localhost.localdomain>
2020-07-04 05:54:09 +0000

    [Add] deviseのコントローラを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,22 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	devise_for :members
+  devise_for :members, controllers: {
+    sessions: 'members/sessions',
+    passwords: 'members/passwords',
+    registrations: 'members/registrations'
+  }
 
-	resources :members, except: [:new, :create, :destroy] do
-		get :confirm, on: :collection
-		patch '/hide' => 'members#hide', on: :member
-	end
+  resources :members, except: [:new, :create, :destroy] do
+    get :confirm, on: :collection
+    patch '/hide' => 'members#hide', on: :member
+  end
 
-	resources :articles do
-		resources :comments, only: [:create, :destroy]
-		resource :favorites, only: [:create, :destroy]
-	end
+  resources :articles do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 
-	get '/about' => 'home#about'
-	root 'home#top'
+  get '/about' => 'home#about'
+  root 'home#top'
 end
