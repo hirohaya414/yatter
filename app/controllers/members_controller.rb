6bc2f3ad46b4b2e894ebce8825f1cb2c40fe5490
vagrant <vagrant@localhost.localdomain>
2020-07-04 05:46:39 +0000

    [Add] 退会機能を追加する

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,27 +3,36 @@ class MembersController < ApplicationController
   end
 
   def show
-  	@member = Member.find(params[:id])
+    @member = Member.find(params[:id])
   end
 
   def edit
-  	@member = Member.find(params[:id])
+    @member = Member.find(params[:id])
   end
 
   def update
-  	@member = Member.find(params[:id])
-  	if @member.update(member_params)
-		redirect_to member_path(@member)
-	else
-		render :edit
-	end
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to member_path(@member)
+    else
+      render :edit
+  end
   end
 
   def confirm
   end
 
+  def hide
+  	@member = Member.find(params[:id])
+  	@member.update(is_deleted: true)
+  	reset_session
+    flash[:notice] = 'ありがとうございました。またのご利用を心よりお待ちしております。'
+  	redirect_to root_path
+  end
+
   private
+
   def member_params
-  	params.require(:member).permit(:profile_image, :name, :school_year, :email)
+    params.require(:member).permit(:profile_image, :name, :school_year, :email)
   end
 end
